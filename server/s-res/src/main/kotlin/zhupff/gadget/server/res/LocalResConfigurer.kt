bbf4d6a28ed48c6c47f494ef8131ab7348af7d46
Zhupf <zhupff@qq.com>
2024-10-29 16:32:16 +0800

    添加本地资源映射

--- server/s-res/src/main/kotlin/zhupff/gadget/server/res/LocalResConfigurer.kt
+++ server/s-res/src/main/kotlin/zhupff/gadget/server/res/LocalResConfigurer.kt
@@ -3,12 +3,22 @@ package zhupff.gadget.server.res
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.io.File
 
 @Configuration
 class LocalResConfigurer : WebMvcConfigurer {
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("/res/**")
-            .addResourceLocations("file:D:/GadgetServerResource/")
+        try {
+            val localResourceDir = File("").resolve("localres")
+            val checkFile = localResourceDir.resolve("localres.txt")
+            val exists = checkFile.readText().isNotEmpty()
+            if (exists) {
+                registry.addResourceHandler("/res/**")
+                    .addResourceLocations("file:${localResourceDir.absolutePath}/")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
